8071d256cb1ebe2d9329d0656c9f744022faef02
Anas saifi <8279483892a@gmail.com>
2024-08-12 01:20:44 +0530

    fixing some bugs

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,23 +1,28 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
-import freshBurger from "../../assets/fresh-burger.png";
-import hipsster from "../../assets/hipsster.png";
-import fitLift from "../../assets/fitlift.png";
+
 import ProjectCard from "../../common/ProjectCard";
+import Portfolio from "../../assets/portfpliooics.jpeg";
+import online from "../../assets/online-book-store-free-vector.jpg";
+import Weather from "../../assets/weather-icons.jpeg";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard src={viberr} h3="Viberr" p="Streaming App" />
         <ProjectCard
-          src={freshBurger}
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          link={"https://github.com/anassaifi82/React-weather-app"}
+          src={Weather}
+          h3="Weathet APP"
+          p="weather forcasting"
         />
-        <ProjectCard src={hipsster} h3="Hipsster" p="Glasses Shop" />
-        <ProjectCard src={fitLift} h3="FitLift" p="Fitness App" />
+        <ProjectCard
+          link={"https://github.com/anassaifi82/portfolio-website-using-react"}
+          src={Portfolio}
+          h3=" Portfolio website"
+          p="Personal portolio"
+        />
+        <ProjectCard src={online} h3="Online Book Store" p="book Shop" />
       </div>
     </section>
   );
